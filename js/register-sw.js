7dc0f1ff04e079c5c10c2cd00c03c30a06a20a84
BriandelValle <brian.delvalle@edu.ar>
2021-11-19 21:31:18 -0300

    ruteo ok

--- js/register-sw.js
+++ js/register-sw.js
@@ -1,7 +1,7 @@
 // chequeo si el browser soporta Service Worker
 
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("./service-worker.js").then((message) => {
+    navigator.serviceWorker.register("../service-worker.js").then((message) => {
         console.log("Service Worker esta ATR!");
     });
 } else {
